6c51bb43df58f94843b5a6f0e00465e223a99bba
JK Malra <jaskarans.work@gmail.com>
2026-06-13 18:22:57 +0530

    Add: LastTouchView component implementation

---
+++ src/view/LastTouchView.ts
@@ -0,0 +1,29 @@
+import { ItemView, WorkspaceLeaf } from "obsidian";
+
+export const VIEW_TYPE_LAST_TOUCH = "last-touch-view";
+
+export class LastTouchView extends ItemView {
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf);
+    }
+
+    getViewType(): string {
+        return VIEW_TYPE_LAST_TOUCH;
+    }
+
+    getDisplayText(): string {
+        return "Last Touch";
+    }
+
+    async onOpen() {
+        const container = this.containerEl;
+        
+        container.empty();
+
+        container.createEl("h2", {
+            text: "Last Touch"
+        });
+    }
+
+    async onClose() {}
+}
